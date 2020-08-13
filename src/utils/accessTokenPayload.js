37cc979a9526480268d9a2234e1bff0f9bea60fb
Amit Parihar <aparihar@corp.lpl.com>
2020-08-13 16:13:48 +0530

    typo fix in token payload

--- src/utils/accessTokenPayload.js
+++ src/utils/accessTokenPayload.js
@@ -1,23 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (event) => {
-    var authToken = event.authorizationToken,
-        authTokenArr = authToken.split(" ") || [],
-        bearer = authTokenArr[0] || "",
-        token = authTokenArr[1] || "";
-    
-    if (authHeaderArr.length !== 2 || bearer.toLowerCase() !== "bearer" || token.length === 0){
-        return null;
-    }
-    var decodedToken = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
-    if (decodedToken && decodedToken.uid && decodedToken.uid.length > 0){
-        return decodedToken;
-    }
+  var authToken = event.authorizationToken,
+    authTokenArr = authToken.split(" ") || [],
+    bearer = authTokenArr[0] || "",
+    token = authTokenArr[1] || "";
+
+  if (
+    authTokenArr.length !== 2 ||
+    bearer.toLowerCase() !== "bearer" ||
+    token.length === 0
+  ) {
     return null;
-
-
-    
-
-
-
-}
+  }
+  var decodedToken = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
+  if (decodedToken && decodedToken.uid && decodedToken.uid.length > 0) {
+    return decodedToken;
+  }
+  return null;
+};
